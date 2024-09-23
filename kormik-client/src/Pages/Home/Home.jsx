8c5617ccc6a1b31f20a5bc89a31225e4681d666a
unknown <gazinafisrafi.gnr@gmail.com>
2024-09-23 16:17:00 +0600

    review section initiated

--- kormik-client/src/Pages/Home/Home.jsx
+++ kormik-client/src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Banner from "./Banner/Banner";
 import Brands from "./Brands/Brands";
 import PopularStats from "./PopuplarStats/PopularStats";
+import Reviews from "./Review/Reviews";
 
 const Home = () => {
 
@@ -9,6 +10,7 @@ const Home = () => {
      <Banner/>
      <Brands/>
      <PopularStats/>
+     <Reviews/>
     </section>
   );
 };
---
+++ kormik-client/src/Pages/Home/Review/Reviews.jsx
@@ -0,0 +1,12 @@
+import Heading from "../../../Shared/Heading/Heading";
+
+const Reviews = () => {
+    return (
+        <section className="my-4">
+            <Heading>what people say about us</Heading>
+            {/* will implement soon after MVP */}
+        </section>
+    );
+};
+
+export default Reviews;
