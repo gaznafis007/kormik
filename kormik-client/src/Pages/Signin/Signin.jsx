200d663853154073f64a8c0200de795c14db8e1e
unknown <gazinafisrafi.gnr@gmail.com>
2024-09-23 22:10:53 +0600

    login page initiated

--- kormik-client/src/Pages/Signin/Signin.jsx
+++ kormik-client/src/Pages/Signin/Signin.jsx
@@ -1,11 +1,20 @@
-
+import Heading from "../../Shared/Heading/Heading";
 
 const Signin = () => {
-    return (
-        <section>
-            <h2 className="text-5xl text-red-400 text-center">This is Sign in page</h2>
-        </section>
-    );
+  return (
+    <section>
+      <Heading>login here to get over with your projects</Heading>
+      <form className="bg-gray-800 mx-12 p-12 rounded-md shadow-md shadow-slate-600">
+        <div>
+          <input type="email" name="email" placeholder="Your email" id="" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="password" id="" />
+        </div>
+        <input type="submit" value="Log in" className="my-2 bg-rose-500 p-3 text-white font-sans font-semibold rounded-md mx-auto hover:bg-rose-600 " />
+      </form>
+    </section>
+  );
 };
 
 export default Signin;
