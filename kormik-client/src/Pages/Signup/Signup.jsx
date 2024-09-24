757758a252ff068430060e5ec8c57147512038c6
unknown <gazinafisrafi.gnr@gmail.com>
2024-09-24 07:22:35 +0600

    client and talents user info form created

--- kormik-client/src/Pages/Signup/Signup.jsx
+++ kormik-client/src/Pages/Signup/Signup.jsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
 import Heading from "../../Shared/Heading/Heading";
+import InputField from "../../Shared/InputField/InputField";
+import useAxiosForData from "../../hooks/useAxiosForData/useAxiosForData";
 
 const Signup = () => {
   const [role, setRole] = useState("");
+  const [categories] = useAxiosForData("/categories");
+  // console.log(categories);
   const handleRole = (event) => {
     event.preventDefault();
     console.log(event.target.value);
@@ -11,56 +15,77 @@ const Signup = () => {
   const handleEmployerRegister = (event) => {
     event.preventDefault();
     const form = event.target;
-    const user = {
+    const formData = new FormData();
+    const profileImage = form.profileImage.files[0];
+    formData.append("employerProfileImage", profileImage);
+    const employer = {
       name: form.name.value,
       email: form.email.value,
       password: form.password.value,
       role,
+      employerCompanyName: form.companyName.value,
+      employerCompanyAddress: form.companyAddress.value,
+      userURL: form.companyWebsite.value,
+      employerIndustry: form.industry.value,
+      employerCompanySize: form.companySize.value,
+      country: form.country.value,
+      companyBio: form.companyBio.value,
+      bio: form.employerBio.value,
+      paymentMethod: form.paymentMethod.value,
+      userProfileImage: formData,
     };
-    console.log("employer", user);
+
+    console.log(employer);
   };
-  const handleFreelancerRegister = event =>{
+  const handleFreelancerRegister = (event) => {
     event.preventDefault();
     const form = event.target;
+    const formData = new FormData();
+    formData.append("profileImage", form.profileImage.files[0]);
     const user = {
       name: form.name.value,
       email: form.email.value,
       password: form.password.value,
       role,
+      title: form.title.value,
+      category: form.category.value,
+      skills: form.skills.value,
+      bio: form.bio.value,
+      userURL: form.userURL.value,
+      paymentMethod: form.paymentMethod.value,
+      country: form.country.value,
+      rate: form.rate.value,
+      availability: form.availability.value,
+      userProfileImage: formData,
     };
-    console.log("freelaner", user);
-  }
+    console.log("freelancer", user);
+  };
   return (
     <section>
       <Heading>welcome! land your first project with us</Heading>
       <form
-        onSubmit={role === 'employer' ? handleEmployerRegister : handleFreelancerRegister}
+        onSubmit={
+          role === "employer"
+            ? handleEmployerRegister
+            : handleFreelancerRegister
+        }
         className="bg-gray-800 flex flex-col gap-4 md:w-1/2 mx-4 md:mx-auto rounded-md shadow-md shadow-slate-600 p-8"
       >
-        <div className="flex flex-col gap-4">
-          <label className="capitalize text-rose-500">your name</label>
-          <input
-            type="text"
-            name="name"
-            className="w-full p-2 rounded-md border border-rose-500"
-          />
-        </div>
-        <div className="flex flex-col gap-4">
-          <label className="capitalize text-rose-500">your email</label>
-          <input
-            type="email"
-            name="email"
-            className="w-full p-2 rounded-md border border-rose-500"
-          />
-        </div>
-        <div className="flex flex-col gap-4">
-          <label className="capitalize text-rose-500">your password</label>
-          <input
-            type="password"
-            name="password"
-            className="w-full p-2 rounded-md border border-rose-500"
-          />
-        </div>
+        <InputField
+          label={"your name"}
+          inputType={"text"}
+          inputName={"name"}
+        ></InputField>
+        <InputField
+          label={"your email"}
+          inputType={"email"}
+          inputName={"email"}
+        ></InputField>
+        <InputField
+          label={"your password"}
+          inputType={"password"}
+          inputName={"password"}
+        ></InputField>
         <div className="flex flex-row gap-4 items-center">
           <label className="capitalize text-rose-500">who are you?</label>
           <select
@@ -68,101 +93,190 @@ const Signup = () => {
             name="role"
             className="w-1/3 p-2 rounded-md border border-rose-500"
           >
-            <option value="" selected>
+            <option value={""} selected>
               Select your role
             </option>
             <option value="employer">Employer</option>
             <option value="freelancer">Freelancer</option>
           </select>
         </div>
-        {role === "employer" ? (
+        {role === "employer" && (
           <>
+            <InputField
+              label={"company name"}
+              inputType={"text"}
+              inputName={"companyName"}
+            ></InputField>
+            <InputField
+              label={"company address"}
+              inputType={"text"}
+              inputName={"companyAddress"}
+            ></InputField>
+            <InputField
+              label={"company website"}
+              inputType={"url"}
+              inputName={"companyWebsite"}
+            ></InputField>
+            <InputField
+              label={"industry"}
+              inputType={"text"}
+              inputName={"industry"}
+            ></InputField>
             <div className="flex flex-col gap-4">
-            <label className="capitalize text-rose-500">company name</label>
-            <input
-              type="text"
-              name="companyName"
-              className="w-full p-2 rounded-md border border-rose-500"
-            />
-          </div>
-            <div className="flex flex-col gap-4">
-            <label className="capitalize text-rose-500">company address</label>
-            <input
-              type="text"
-              name="companyAddress"
-              className="w-full p-2 rounded-md border border-rose-500"
-            />
-          </div>
-            <div className="flex flex-col gap-4">
-            <label className="capitalize text-rose-500">company website</label>
-            <input
-              type="url"
-              name="companyWebsite"
-              className="w-full p-2 rounded-md border border-rose-500"
-            />
-          </div>
-            <div className="flex flex-col gap-4">
-            <label className="capitalize text-rose-500">Industry</label>
-            <input
-              type="text"
-              name="industry"
-              className="w-full p-2 rounded-md border border-rose-500"
-            />
-          </div>
-            <div className="flex flex-col gap-4">
-            <label className="capitalize text-rose-500">Business size</label>
-            <select name="companySize" className="w-full p-2 rounded-md border border-rose-500">
+              <label className="capitalize text-rose-500">Business size</label>
+              <select
+                name="companySize"
+                className="w-full p-2 rounded-md border border-rose-500"
+              >
                 <option selected>Select your company size</option>
                 <option value={"small"}>{"Small (<10 employee)"}</option>
                 <option value={"medium"}>{"Medium (100+ employee)"}</option>
                 <option value={"large"}>{"Large (1001+ employee)"}</option>
-            </select>
-          </div>
-          <div className="flex flex-col gap-4">
-            <label className="capitalize text-rose-500">Country</label>
-            <input
-              type="text"
-              name="country"
-              className="w-full p-2 rounded-md border border-rose-500"
-            />
-          </div>
-          <div className="flex flex-col gap-4">
-            <label className="capitalize text-rose-500">description on your company</label>
-            <textarea
-              type="text"
-              name="companyBio"
-              className="w-full p-6 rounded-md border border-rose-500"
-            />
-          </div>
-          <div className="flex flex-col gap-4">
-            <label className="capitalize text-rose-500">tell us about yourself</label>
-            <textarea
-              type="text"
-              name="employerBio"
-              className="w-full p-6 rounded-md border border-rose-500"
-            />
-          </div>
-          <div className="flex flex-col gap-4">
-            <label className="capitalize text-rose-500">preferred payment method</label>
-            <select name="paymentMethod" className="w-full p-2 rounded-md border border-rose-500">
+              </select>
+            </div>
+            <InputField
+              label={"country"}
+              inputType={"text"}
+              inputName={"country"}
+            ></InputField>
+            <div className="flex flex-col gap-4">
+              <label className="capitalize text-rose-500">
+                description on your company
+              </label>
+              <textarea
+                type="text"
+                name="companyBio"
+                className="w-full p-6 rounded-md border border-rose-500"
+              />
+            </div>
+            <div className="flex flex-col gap-4">
+              <label className="capitalize text-rose-500">
+                tell us about yourself
+              </label>
+              <textarea
+                type="text"
+                name="employerBio"
+                className="w-full p-6 rounded-md border border-rose-500"
+              />
+            </div>
+            <div className="flex flex-col gap-4">
+              <label className="capitalize text-rose-500">
+                preferred payment method
+              </label>
+              <select
+                name="paymentMethod"
+                className="w-full p-2 rounded-md border border-rose-500"
+              >
                 <option selected>Select your preferred method</option>
                 <option value={"paypal"}>Paypal</option>
                 <option value={"stripe"}>Stripe</option>
                 <option value={"payoneer"}>Payoneer</option>
                 <option value={"bankTransfer"}>Bank transfer</option>
-            </select>
-          </div>
-          <div className="flex flex-col gap-4">
-            <label className="capitalize text-rose-500">profile picture</label>
-            <input
-              type="file"
-              name="profileImage"
-              className="w-full p-2 rounded-md bg-white border border-rose-500"
-            />
-          </div>
+              </select>
+            </div>
+            <div className="flex flex-col gap-4">
+              <label className="capitalize text-rose-500">
+                profile picture
+              </label>
+              <input
+                type="file"
+                name="profileImage"
+                className="w-full p-2 rounded-md bg-white border border-rose-500"
+              />
+            </div>
           </>
-        ) : (
-          ""
+        )}
+        {role === "freelancer" && (
+          <>
+            <InputField
+              label={"your title (work specific)"}
+              inputType={"text"}
+              inputName={"title"}
+            ></InputField>
+            <div className="flex flex-col gap-4">
+              <label className="capitalize text-rose-500">category</label>
+              <select
+                name="category"
+                className="w-full p-2 rounded-md border border-rose-500"
+              >
+                <option selected>Select your category</option>
+                {categories.map((category) => (
+                  <option className="capitalize" key={category._id}>
+                    {category.category}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="flex flex-col gap-4">
+              <label className="capitalize text-rose-500">Skills</label>
+              <textarea
+                type="text"
+                name="skills"
+                className="w-full p-6 rounded-md border border-rose-500"
+              />
+            </div>
+            <div className="flex flex-col gap-4">
+              <label className="capitalize text-rose-500">
+                tell us about yourself
+              </label>
+              <textarea
+                type="text"
+                name="bio"
+                className="w-full p-6 rounded-md border border-rose-500"
+              />
+            </div>
+            <InputField
+              label={"Website/Portfolios/URLs"}
+              inputType={"url"}
+              inputName={"userURL"}
+            ></InputField>
+            <div className="flex flex-col gap-4">
+              <label className="capitalize text-rose-500">
+                preferred payment method
+              </label>
+              <select
+                name="paymentMethod"
+                className="w-full p-2 rounded-md border border-rose-500"
+              >
+                <option selected>Select your preferred method</option>
+                <option value={"paypal"}>Paypal</option>
+                <option value={"stripe"}>Stripe</option>
+                <option value={"payoneer"}>Payoneer</option>
+                <option value={"bankTransfer"}>Bank transfer</option>
+              </select>
+            </div>
+            <InputField
+              label={"country"}
+              inputType={"text"}
+              inputName={"country"}
+            ></InputField>
+            <InputField
+              label={"hourly rate"}
+              inputType={"text"}
+              inputName={"rate"}
+            ></InputField>
+            <div className="flex flex-col gap-4">
+              <label className="capitalize text-rose-500">availability</label>
+              <select
+                name="availability"
+                className="w-full p-2 rounded-md border border-rose-500"
+              >
+                <option selected>Select your preferred method</option>
+                <option value={"full-time"}>Full time</option>
+                <option value={"part-time"}>Part time</option>
+              </select>
+            </div>
+            <div className="flex flex-col gap-4">
+              <label className="capitalize text-rose-500">
+                profile picture
+              </label>
+              <input
+                type="file"
+                name="profileImage"
+                className="w-full p-2 rounded-md bg-white border border-rose-500"
+              />
+            </div>
+          </>
         )}
         {role && (
           <input
---
+++ kormik-client/src/Shared/InputField/InputField.jsx
@@ -0,0 +1,16 @@
+
+// eslint-disable-next-line react/prop-types
+const InputField = ({label, inputType, inputName}) => {
+  return (
+    <div className="flex flex-col gap-4">
+      <label className="capitalize text-rose-500">{label}</label>
+      <input
+        type={inputType}
+        name={inputName}
+        className="w-full p-2 rounded-md border border-rose-500"
+      />
+    </div>
+  );
+};
+
+export default InputField;
