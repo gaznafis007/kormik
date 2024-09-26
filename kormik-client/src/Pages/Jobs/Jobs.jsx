2449b707e8bc0060bbbe9a199b92f4f23fa9bec3
unknown <gazinafisrafi.gnr@gmail.com>
2024-09-27 02:08:19 +0600

    route created with basic ui and card created with minimal infoof skill

--- kormik-client/src/Pages/Jobs/Jobs.jsx
+++ kormik-client/src/Pages/Jobs/Jobs.jsx
@@ -1,22 +1,52 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import useAxios from "../../hooks/useAxios/useAxios";
 import Heading from "../../Shared/Heading/Heading";
+import Card from "../../Shared/Card/Card";
+import { ArrowLongRightIcon } from "@heroicons/react/24/outline";
 
 const Jobs = () => {
-    const [jobs, setJobs] = useState([])
-    const axiosSecure = useAxios();
-    useEffect(() =>{
-        axiosSecure.get("/jobs")
-        .then(res =>{
-            setJobs(res.data)
-        })
-    },[])
-    return (
-        <div>
-            <Heading>Find job page is landing soon...</Heading>
-            <h2 className="my-8 text-rose-600 text-3xl text-center font-semibold font-sans"> Total Job: {jobs.length}</h2>
-        </div>
-    );
+  const [jobs, setJobs] = useState([]);
+  const axiosSecure = useAxios();
+  useEffect(() => {
+    axiosSecure.get("/jobs").then((res) => {
+      setJobs(res.data);
+    });
+  }, []);
+  return (
+    <div>
+      <Heading>Find job page is landing soon...</Heading>
+      <h2 className="my-8 text-rose-600 text-3xl text-center font-semibold font-sans">
+        {" "}
+        Total Job: {jobs.length}
+      </h2>
+      <div className="grid mx-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 my-6">
+        {jobs.map((job) => (
+          <Card key={job._id} title={job?.title}>
+            Rate: {job?.projectRate}
+            <br />
+            <div className="flex flex-row gap-2 items-center my-4">
+              {job?.skill.map((skill, idx) => (
+                <p
+                  key={idx}
+                  className="bg-slate-700 px-2 py-1 text-sm rounded-full text-center"
+                >
+                  {skill}
+                </p>
+              ))}
+            </div>
+            <Link
+              to={`/jobs/${job._id}`}
+              className="flex flex-row gap-3 justify-between items-center text-rose-500 my-4"
+            >
+              Checkout{" "}
+              <ArrowLongRightIcon className="size-6 text-rose-500 hover:size-7"></ArrowLongRightIcon>
+            </Link>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Jobs;
--- kormik-client/src/Shared/Card/Card.jsx
+++ kormik-client/src/Shared/Card/Card.jsx
@@ -1,15 +1,13 @@
-
-
 // eslint-disable-next-line react/prop-types
-const Card = ({title, children}) => {
-    return (
-        <div className="p-6 bg-slate-900 rounded-md">
-                    <h2 className="text-lg text-rose-500 font-semibold font-sans capitalize">{title}</h2>
-                    <p className="py-4 text-white font-semibold font-sans">
-                        {children}
-                    </p>
-                </div>
-    );
+const Card = ({ title, children }) => {
+  return (
+    <div className="p-6 bg-slate-900 rounded-md">
+      <h2 className="text-lg text-rose-500 font-semibold font-sans capitalize">
+        {title}
+      </h2>
+      <p className="py-4 text-white font-semibold font-sans">{children}</p>
+    </div>
+  );
 };
 
 export default Card;
