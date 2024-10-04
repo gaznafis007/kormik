c27f0cfd3e66cc5d89e106757f5d812d85284205
unknown <gazinafisrafi.gnr@gmail.com>
2024-10-04 18:05:33 +0600

    jobsearch ui fixing

--- kormik-client/src/Pages/Job/JobSearch/JobSearch.jsx
+++ kormik-client/src/Pages/Job/JobSearch/JobSearch.jsx
@@ -2,10 +2,15 @@
 import InputField from "../../../Shared/InputField/InputField";
 import InputSubmitForForm from "../../../Shared/InputSubmitForForm/InputSubmitForForm";
 
-
-const JobSearch = ({ categories, subCategories, handleSubCategories, isLoading, handleSearch, handleTitleSearch, handleJobTypeSearch }) => {
-    
-
+const JobSearch = ({
+  categories,
+  subCategories,
+  handleSubCategories,
+  isLoading,
+  handleSearch,
+  handleTitleSearch,
+  handleJobTypeSearch,
+}) => {
   if (isLoading) {
     return (
       <h2 className="text-center text-2xl text-rose-500 font-sans font-semibold animate-ping">
@@ -16,67 +21,71 @@ const JobSearch = ({ categories, subCategories, handleSubCategories, isLoading, 
   return (
     <form onSubmit={handleSearch} className="p-4 my-6">
       <div className="grid grid-col-1 md:grid-cols-3 gap-4">
-      <InputField
-        handler={handleTitleSearch}
-        inputName={"title"}
-        inputType={"text"}
-        label={"job title"}
-      ></InputField>
-      <div className="flex flex-col my-2 gap-4">
-        <label className="capitalize text-rose-500">
-          Choose project category
-        </label>
-        <select
-          name="category"
-          className="w-full p-2 rounded-md border border-rose-500"
-          onChange={handleSubCategories}
-        >
-          <option value={''} selected>Chose your project category</option>
-          {categories &&
-            categories?.map((category) => (
-              <option
-                key={category._id}
-                value={category.category}
-                className="capitalize"
-              >
-                {category.category}
-              </option>
-            ))}
-        </select>
-      </div>
-      {subCategories && (
-            <div className="flex flex-col my-2 gap-4">
-              <label className="capitalize text-rose-500">Sub category</label>
-              <select
-                name="subCategory"
-                className="w-full p-2 rounded-md border border-rose-500"
-              >
-                <option value={''} selected>Chose your project sub-category</option>
-                {
-                subCategories?.subCategories?.map((subCategory, idx) => (
-                  <option key={idx} className="capitalize" value={subCategory}>
-                    {subCategory}
-                  </option>
-                ))}
-              </select>
-            </div>
-          )
-        }
-      </div>
-      <div className="flex flex-col my-2 gap-4 w-full md:w-1/3">
-            <label className="capitalize text-rose-500">job type</label>
+        <InputField
+          handler={handleTitleSearch}
+          inputName={"title"}
+          inputType={"text"}
+          label={"job title"}
+        ></InputField>
+        <div className="flex flex-col my-2 gap-4">
+          <label className="capitalize text-rose-500">
+            Choose project category
+          </label>
+          <select
+            name="category"
+            className="w-full p-2 rounded-md border border-rose-500"
+            onChange={handleSubCategories}
+          >
+            <option value={""} selected>
+              Chose your project category
+            </option>
+            {categories &&
+              categories?.map((category) => (
+                <option
+                  key={category._id}
+                  value={category.category}
+                  className="capitalize"
+                >
+                  {category.category}
+                </option>
+              ))}
+          </select>
+        </div>
+        {subCategories && (
+          <div className="flex flex-col my-2 gap-4">
+            <label className="capitalize text-rose-500">Sub category</label>
             <select
-              name="jobType"
+              name="subCategory"
               className="w-full p-2 rounded-md border border-rose-500"
-              onChange={handleJobTypeSearch}
             >
-              <option selected value={''}>Chose job type</option>
-              <option value={"fullTime"}>Full time</option>
-              <option value={"project"}>Project base</option>
-              <option value={"partTime"}>Part time</option>
+              <option value={""} selected>
+                Chose your project sub-category
+              </option>
+              {subCategories?.subCategories?.map((subCategory, idx) => (
+                <option key={idx} className="capitalize" value={subCategory}>
+                  {subCategory}
+                </option>
+              ))}
             </select>
           </div>
-        <InputSubmitForForm type={"submit"} value={"search"}></InputSubmitForForm>
+        )}
+        <div className="flex flex-col my-2 gap-4">
+          <label className="capitalize text-rose-500">job type</label>
+          <select
+            name="jobType"
+            className="w-full p-2 rounded-md border border-rose-500"
+            onChange={handleJobTypeSearch}
+          >
+            <option selected value={""}>
+              Chose job type
+            </option>
+            <option value={"fullTime"}>Full time</option>
+            <option value={"project"}>Project base</option>
+            <option value={"partTime"}>Part time</option>
+          </select>
+        </div>
+      </div>
+      <InputSubmitForForm type={"submit"} value={"search"}></InputSubmitForForm>
     </form>
   );
 };
