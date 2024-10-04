64636f80aa8e8d19b6e6b94d740ac1911b31e102
unknown <gazinafisrafi.gnr@gmail.com>
2024-10-04 19:26:40 +0600

    footer polishing

--- kormik-client/src/Shared/Footer/Footer.jsx
+++ kormik-client/src/Shared/Footer/Footer.jsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
     return (
-        <footer className="absolute bottom-0 bg-slate-700 w-full">
+        <footer className="bottom-0 bg-slate-700 w-full mt-6">
             <h2 className="text-3xl text-center text-gray-200 font-semibold">This is dummy footer</h2>
         </footer>
     );
