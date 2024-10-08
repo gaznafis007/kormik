2a78122ac9abc0f08fe3cccc0698151b0745831c
unknown <gazinafisrafi.gnr@gmail.com>
2024-10-09 04:36:53 +0600

    project textig implementing piece by piece

--- kormik-client/src/Pages/Job/ProjectTexting/ProjectTexting.jsx
+++ kormik-client/src/Pages/Job/ProjectTexting/ProjectTexting.jsx
@@ -1,10 +1,19 @@
+import useAuth from "../../../hooks/useAuth/useAuth";
+import Button from "../../../Shared/Button/Button";
 import Heading from "../../../Shared/Heading/Heading";
 
 
 const ProjectTexting = () => {
+    const {user} = useAuth()
     return (
         <div className="p-6 w-full md:w-1/3 overflow-y-auto border-2 rounded-md border-slate-900 shadow-md shadow-slate-300">
             <Heading>project details</Heading>
+            {
+                user?.email === 'employer' ? 
+                <Button>Accept the project file</Button>
+                :
+                <Button>submit project file</Button>
+            }
         </div>
     );
 };
