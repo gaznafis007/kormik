3504750a2591ea03f36266f8db17a1290e7a84d5
unknown <gazinafisrafi.gnr@gmail.com>
2024-10-18 18:49:29 +0600

    integrating socket.io

--- kormik-server/index.js
+++ kormik-server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
+const http = require('http')
 const app = express();
 const data  = require('./data/categories.json')
 
